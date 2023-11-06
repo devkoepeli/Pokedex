cc2871dd52f13f7f41d030463ea0c53042c31f47
devkoepeli <devrim_kopeli@hotmail.com>
2023-11-06 21:45:18 +0100

    types + bg color

--- script.js
+++ script.js
@@ -6,6 +6,28 @@ let pokemonNames = [
 ];
 
 
+let pokemonTypes = [
+    { type: "normal", color: "#A8A878" },
+    { type: "fire", color: "#F08030" },
+    { type: "water", color: "#6890F0" },
+    { type: "electric", color: "#F8D030" },
+    { type: "grass", color: "#78C850" },
+    { type: "ice", color: "#98D8D8" },
+    { type: "fighting", color: "#C03028" },
+    { type: "poison", color: "#A040A0" },
+    { type: "ground", color: "#E0C068" },
+    { type: "flying", color: "#A890F0" },
+    { type: "psychic", color: "#F85888" },
+    { type: "bug", color: "#A8B820" },
+    { type: "rock", color: "#B8A038" },
+    { type: "ghost", color: "#705898" },
+    { type: "steel", color: "#B8B8D0" },
+    { type: "dragon", color: "#7038F8" },
+    { type: "dark", color: "#705848" },
+    { type: "fairy", color: "#EE99AC" }
+];
+
+
 let pokemonJSON = [];
 
 
@@ -26,13 +48,31 @@ document.addEventListener('DOMContentLoaded', getPokemon);
 function renderPokemon(i) {
     let currentName = pokemonJSON[i]['name'].charAt(0).toUpperCase() + pokemonJSON[i]['name'].slice(1);
     let currentImage = pokemonJSON[i]['sprites']['front_default'];
-    document.getElementById('content').innerHTML += pokemonHTML(currentName, currentImage, i);
+    let currentType = pokemonJSON[i]['types'][0]['type']['name'];
+    document.getElementById('content').innerHTML += pokemonHTML(currentName, currentImage, currentType, i);
+    colorElement(i);
+}
+
+
+function colorElement(index) {
+    let pokemonCard = document.getElementById(`pokemon${index}`);
+    let pokemonType = document.getElementById(`type${index}`).innerHTML;
+
+    for (let i = 0; i < pokemonTypes.length; i++) {
+        const type = pokemonTypes[i]['type'];
+        const color = pokemonTypes[i]['color'];
+        if (pokemonType === type) {
+            pokemonCard.style.backgroundColor = color;
+            return;
+        }
+    }
 }
 
 
 function openModal(index) {
     let modal = document.getElementById('modal');
-    modal.style.display = 'block';
+    modal.classList.add('d-block');
+    document.body.classList.add('o-hidden'); // disable scroll
     renderPokedex(index);
     modal.addEventListener('click', function(event) {
         // console.log(event.target); // überprüfe um welches Element es sich handelt beim click
@@ -42,16 +82,19 @@ function openModal(index) {
 
 
 function renderPokedex(index) {
-    let pokemon = pokemonJSON[index];
+    const pokemon = pokemonJSON[index];
     const namePokemon = pokemon['name'].charAt(0).toUpperCase() + pokemon['name'].slice(1);
+    const typePokemon = pokemon['types'][0]['type']['name']; // gibt den Typen des angeclickten Pokemons zurück z.B. "fire"
     const imagePokemon = pokemon['sprites']['front_default'];
     const heightPokemon = pokemon['height'];
     const weightPokemon = pokemon['weight'];
     const movePokemon = pokemon['moves'][0]['move']['name'];
     document.getElementById('modal').innerHTML = `
         <div class="pokedex-container" id="pokedex-container">
-            <div class="pokedex-details" id="pokedex-content">
+            <div class="pokedex-details ${typePokemon}" id="pokedex-content">
                 <h2 class="pokedex-title">${namePokemon}</h2>
+                <span class="pokedex-type">${typePokemon}</span>
+                <span class="pokedex-number">#${String(index + 1).padStart(3, '0')}</span>
                 <div class="pokedex-img-container">
                     <img class="pokedex-img" src="${imagePokemon}" alt="">
                 </div>
@@ -87,7 +130,8 @@ function closeModal(event) {
     // überprüfe ob auf den Hintergrund geclickt wurde
     if (event.target === modal || event.target === pokedexContainer) { // ist das ziel des clicks dasselbe Element, auf das geclickt wurde
         modal.innerHTML = '';
-        modal.style.display = 'none';
+        modal.classList.remove('d-block');
+        document.body.classList.remove('o-hidden');
     }
 }
 
@@ -100,20 +144,22 @@ function searchPokemon() {
 
     for (let i = 0; i < pokemonNames.length; i++) {
         const pokemonName = pokemonNames[i]; // by checking each name in the array we make sure it renders immediately the pokemon
-        if (pokemonName.includes(search)) {
+        if (pokemonName.includes(search)) { // if the element at the position i contains searchvalue (true) render the element
             renderPokemon(i); // passing the current i of the loop
         }
     }
 }
 
 
-function pokemonHTML(name, image, index) {
+function pokemonHTML(name, image, type, index) {
     return `
-        <div id="pokemon" onclick="openModal(${index})">
+        <div id="pokemon${index}" class="pokemon" onclick="openModal(${index})">
             <h2 id="pokemon-name">${name}</h2>
+            <span class="pokemon-element" id="type${index}">${type}</span>
             <div id="pokemon-img-container">
                 <img class="pokemon-img" src="${image}" alt="Bild von Pokemon">
             </div>
+            <span class="pokemon-number">#${String(index + 1).padStart(3, '0')}</span>
         </div>  
     `;
 }
