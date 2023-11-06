d66675fbd149b26bb5b0d2ebf553605ea7c8450d
devkoepeli <devrim_kopeli@hotmail.com>
2023-11-06 22:06:54 +0100

    data in extra file

---
+++ data.js
@@ -0,0 +1,39 @@
+let pokemonNames = [
+    'pikachu', 'charmander', 'ditto', 'bulbasaur', 'charmeleon',
+    'squirtle', 'jigglypuff', 'snorlax', 'eevee', 'mewtwo',
+    'magikarp', 'gengar', 'lapras', 'machop', 'onix',
+    'vaporeon', 'psyduck', 'meowth', 'clefairy', 'geodude',
+    'rattata', 'ekans', 'sandshrew', 'vulpix', 'paras',
+    'venonat', 'dugtrio', 'persian', 'golduck', 'mankey',
+    'growlithe', 'poliwag', 'poliwhirl', 'abra', 'kadabra',
+    'alakazam', 'machoke', 'machamp', 'bellsprout', 'weepinbell',
+    'victreebel', 'tentacool', 'tentacruel', 'graveler', 'golem',
+    'ponyta', 'rapidash', 'slowpoke', 'slowbro', 'magnemite',
+    'magneton', 'farfetchd', 'doduo', 'seel', 'dewgong',
+    'grimer', 'haunter', 'gengar', 'onix', 'drowzee'
+];
+
+
+let pokemonTypes = [
+    { type: "normal", color: "#A8A878" },
+    { type: "fire", color: "#F08030" },
+    { type: "water", color: "#6890F0" },
+    { type: "electric", color: "#F8D030" },
+    { type: "grass", color: "#78C850" },
+    { type: "ice", color: "#98D8D8" },
+    { type: "fighting", color: "#C03028" },
+    { type: "poison", color: "#A040A0" },
+    { type: "ground", color: "#E0C068" },
+    { type: "flying", color: "#A890F0" },
+    { type: "psychic", color: "#F85888" },
+    { type: "bug", color: "#A8B820" },
+    { type: "rock", color: "#B8A038" },
+    { type: "ghost", color: "#705898" },
+    { type: "steel", color: "#B8B8D0" },
+    { type: "dragon", color: "#7038F8" },
+    { type: "dark", color: "#705848" },
+    { type: "fairy", color: "#EE99AC" }
+];
+
+
+let pokemonJSON = [];
--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-let pokemonNames = [
-    'pikachu', 'charmander', 'ditto', 'bulbasaur', 'charmeleon',
-    'squirtle', 'jigglypuff', 'snorlax', 'eevee', 'mewtwo',
-    'magikarp', 'gengar', 'lapras', 'machop', 'onix',
-    'vaporeon', 'psyduck', 'meowth', 'abra', 'geodude'
-];
-
-
-let pokemonTypes = [
-    { type: "normal", color: "#A8A878" },
-    { type: "fire", color: "#F08030" },
-    { type: "water", color: "#6890F0" },
-    { type: "electric", color: "#F8D030" },
-    { type: "grass", color: "#78C850" },
-    { type: "ice", color: "#98D8D8" },
-    { type: "fighting", color: "#C03028" },
-    { type: "poison", color: "#A040A0" },
-    { type: "ground", color: "#E0C068" },
-    { type: "flying", color: "#A890F0" },
-    { type: "psychic", color: "#F85888" },
-    { type: "bug", color: "#A8B820" },
-    { type: "rock", color: "#B8A038" },
-    { type: "ghost", color: "#705898" },
-    { type: "steel", color: "#B8B8D0" },
-    { type: "dragon", color: "#7038F8" },
-    { type: "dark", color: "#705848" },
-    { type: "fairy", color: "#EE99AC" }
-];
-
-
-let pokemonJSON = [];
-
-
 async function getPokemon() {
     for (let i = 0; i < pokemonNames.length; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${pokemonNames[i]}`;
